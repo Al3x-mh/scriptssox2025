eda9be54a1137dda0b1336edc8343f4a920b9ce0
Al3x-mh <alexandruionut22122007@gmail.com>
2025-11-21 19:41:38 +0100

    prueba

--- scriptrepaso1.sh
+++ scriptrepaso1.sh
@@ -5,12 +5,12 @@ set -u
 set -o pipefail
 
 check_cmd() {
-if ! command -v {$1} >/dev/null;then
+if ! command -v ${1} >/dev/null;then
      echo "Este comando no existe."
      exit 1
 fi
 
-if command -v {$1} >/dev/null;then
+if command -v ${1} >/dev/null;then
     return 0
 fi
 }
